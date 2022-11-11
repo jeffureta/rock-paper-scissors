d31c2004a4432c17df127df763aebbde6ed4d8f2
jeffureta <jeffvincentureta@gmail.com>
2022-11-11 01:08:42 +0000

    Edited index.js
    I've added playerselection() function and is currently working on
    playRound()

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@ function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)]
 }
+function playerSelection(option) {
+    return option.toLowerCase()
+}
+//you're working on this
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && getComputerChoice() === "rock") return "draw"
+}
