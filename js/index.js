564150389d52a2f555b893130e9a0829856b4dc7
jeffureta <jeffvincentureta@gmail.com>
2022-11-11 20:15:09 +0000

    Added playRound() function to index.js.
    playRound(playerSelection, getComputerChoice).
    This function accepts two parameters.
    playerSelection and getComputerChoice which is random.
    This function will return the result of the round.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,15 @@ function playerSelection(option) {
     return option.toLowerCase()
 }
 //you're working on this
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && getComputerChoice() === "rock") return "draw"
+function playRound(playerSelection, getComputerChoice) {
+    //outcomes where player wins
+    if (playerSelection === "rock" && getComputerChoice === "scissors") return `you win ${playerSelection} beats ${getComputerChoice}`
+    if (playerSelection === "paper" && getComputerChoice === "rock") return `you win ${playerSelection} beats ${getComputerChoice}`
+    if (playerSelection === "scissors" && getComputerChoice === "paper") return `you win ${playerSelection} beats ${getComputerChoice}`
+    // //outcomes where computer wins
+    if (getComputerChoice === "rock" && playerSelection === "scissors") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    if (getComputerChoice === "paper" && playerSelection === "rock") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    if (getComputerChoice === "scissors" && playerSelection === "paper") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    // //outcomes where it's a draw
+    else return `draw computer picked ${getComputerChoice}`
 }
