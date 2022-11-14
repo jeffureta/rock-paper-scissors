0cc6e50ebe264ee03a15d77410ecc38d7ceaceee
jeffureta <jeffvincentureta@gmail.com>
2022-11-14 23:14:52 +0000

    Edited js/index.js.
    Removed individual function to get the picks.
    Those were replaced by getPicks() and forEach method.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-function getRock() {
-    return document.querySelectorAll("div")[1]
+function getPicks() {
+    return document.querySelectorAll("[data-selection]")
 }
-function getPaper() {
-    return document.querySelectorAll("div")[2]
-}
-function getScissors() {
-    return document.querySelectorAll("div")[3]
+getPicks().forEach((pick) => {
+    pick.addEventListener("click", (e) => {
+        const pickName = pick.dataset.selection;
+        makeSelection(pickName);
+    })
+})
+function makeSelection(selection) {
+    console.log(selection)
 }
 function getComputerPick() {
     let picksArray = ["✊","🖐️","✌️"];
@@ -17,13 +20,13 @@ function getUserPick(pick) {
 //you're working on this
 function playRound(playerSelection, getComputerChoice) {
     //outcomes where player wins
-    if (playerSelection === "rock" && getComputerChoice === "scissors") return `you win ${playerSelection} beats ${getComputerChoice}`
-    if (playerSelection === "paper" && getComputerChoice === "rock") return `you win ${playerSelection} beats ${getComputerChoice}`
-    if (playerSelection === "scissors" && getComputerChoice === "paper") return `you win ${playerSelection} beats ${getComputerChoice}`
+    // if (playerSelection === "rock" && getComputerChoice === "scissors") return `you win ${playerSelection} beats ${getComputerChoice}`
+    // if (playerSelection === "paper" && getComputerChoice === "rock") return `you win ${playerSelection} beats ${getComputerChoice}`
+    // if (playerSelection === "scissors" && getComputerChoice === "paper") return `you win ${playerSelection} beats ${getComputerChoice}`
     // //outcomes where computer wins
-    if (getComputerChoice === "rock" && playerSelection === "scissors") return `you lose ${getComputerChoice} beats ${playerSelection}`
-    if (getComputerChoice === "paper" && playerSelection === "rock") return `you lose ${getComputerChoice} beats ${playerSelection}`
-    if (getComputerChoice === "scissors" && playerSelection === "paper") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    // if (getComputerChoice === "rock" && playerSelection === "scissors") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    // if (getComputerChoice === "paper" && playerSelection === "rock") return `you lose ${getComputerChoice} beats ${playerSelection}`
+    // if (getComputerChoice === "scissors" && playerSelection === "paper") return `you lose ${getComputerChoice} beats ${playerSelection}`
     // //outcomes where it's a draw
-    else return `draw computer picked ${getComputerChoice}`
+    // else return `draw computer picked ${getComputerChoice}`
 }
