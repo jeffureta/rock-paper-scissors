5a7852dc4568919c10b59d54c3e5deec34ef9bd4
jeffureta <jeffvincentureta@gmail.com>
2022-11-20 18:31:09 +0000

    Edited js/index.js and css/style.css.
    Cleaned index.js.
    As of now, there are 4 functions and 1 event listener.
    The functions are...
    1. allPicks()
    2.queryUserPick()
    3.getComputerPick()
    4. returnResult()

--- js/index.js
+++ js/index.js
@@ -1,32 +1,45 @@
-function getPicks() {
-    return document.querySelectorAll("[data-selection]")
+function allPicks() {
+    return [
+        {pick: "rock",
+        emoji: "✊",
+        beats: "scissors"
+    },
+    {
+        pick: "paper",
+        emoji: "🖐",
+        beats: "rock"
+    },
+    {
+        pick: "scissors",
+        emoji: "✌️",
+        beats: "paper"
+    }
+    ]
 }
-getPicks().forEach((pick) => {
-    pick.addEventListener("click", (e) => {
-        const pickName = pick.dataset.selection;
-        makeSelection(pickName);
-    })
-})
-function makeSelection(selection) {
-    console.log(selection)
+function queryUserPick() {
+    return document.querySelectorAll("[data-pick]")
 }
 function getComputerPick() {
-    let picksArray = ["✊","🖐️","✌️"];
-    return picksArray[Math.floor(Math.random() * picksArray.length)]
+    return allPicks()[Math.floor(Math.random() * allPicks().length)]
 }
-function getUserPick(pick) {
-    return pick
+function returnResult(userPick, comPick) {
+    let result = undefined;
+    //outcomes where user loses
+    if (userPick.pick === comPick.beats) result = -1;
+    //outcomes where user wins
+    if (comPick.pick === userPick.beats) result = 1;
+    //outcomes where it's a draw
+    if (userPick.pick === comPick.pick) result =  0;
+    console.log(result);
+    return result;
 }
-//you're working on this
-function playRound(playerSelection, getComputerChoice) {
-    //outcomes where player wins
-    // if (playerSelection === "rock" && getComputerChoice === "scissors") return `you win ${playerSelection} beats ${getComputerChoice}`
-    // if (playerSelection === "paper" && getComputerChoice === "rock") return `you win ${playerSelection} beats ${getComputerChoice}`
-    // if (playerSelection === "scissors" && getComputerChoice === "paper") return `you win ${playerSelection} beats ${getComputerChoice}`
-    // //outcomes where computer wins
-    // if (getComputerChoice === "rock" && playerSelection === "scissors") return `you lose ${getComputerChoice} beats ${playerSelection}`
-    // if (getComputerChoice === "paper" && playerSelection === "rock") return `you lose ${getComputerChoice} beats ${playerSelection}`
-    // if (getComputerChoice === "scissors" && playerSelection === "paper") return `you lose ${getComputerChoice} beats ${playerSelection}`
-    // //outcomes where it's a draw
-    // else return `draw computer picked ${getComputerChoice}`
-}
+//Event listeners
+queryUserPick().forEach((pick) => {
+    pick.addEventListener("click", () => {
+        const pickName = pick.dataset.pick;
+        const comPick = getComputerPick();
+        const userPick = allPicks().find(i => i.pick === pickName);
+        console.log(userPick, comPick);
+        returnResult(userPick, comPick)
+    })
+})
