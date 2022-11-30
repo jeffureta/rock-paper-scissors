e5d07ff9b7763f73dee0f3ca923dd0dce29eb821
jeffureta <jeffvincentureta@gmail.com>
2022-11-30 15:27:29 +0000

    Edited index.js and added eventListeners.js
    Moved all event listeners from index.js to eventListeners.js

--- js/index.js
+++ js/index.js
@@ -114,36 +114,3 @@ function showPreviousResults (outcome, userPick, comPick) {
     }
 }    
 //other functions end//
-//Event listeners
-queryUserPick().forEach((pick) => {
-    pick.addEventListener("click", () => {
-        roundCount += 1;
-        const pickName = pick.dataset.pick;
-        const comPick = getComputerPick();
-        const userPick = allPicks().find(i => i.pick === pickName)
-        showUserPick(userPick);
-        showComPick(comPick);
-        const newResult = returnResult(userPick, comPick);
-        console.log(`${newResult} this is new result`);
-        if (streakCount <= 0 && newResult[0] === 1) {
-            streakCount = 0;
-            streakElement.innerText = streakCount;
-        }
-        if (streakCount > 0 && newResult[0] === -1) {
-            streakCount = 0;            
-            streakElement.innerText = streakCount;
-        }
-        if (newResult[0] === 1) {
-            streakCount++;
-            streakElement.innerText = streakCount;
-        } else if (newResult[0] === -1) {
-            streakCount += -1;
-            streakElement.innerText = streakCount;
-        } else if (newResult[0] === 0) {
-            streakCount += 0;
-            streakElement.innerText = streakCount;
-        }
-        showPreviousResults(newResult[0], userPick.pick, comPick.pick);
-        returnRound(roundCount);
-    })
-})
